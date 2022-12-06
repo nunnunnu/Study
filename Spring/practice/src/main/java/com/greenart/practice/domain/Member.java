b297832375f03d7b57578d714ab49dea622fa242
jh <bis04199@gmail.com>
2022-12-06 13:53:50 +0900

    221206 L

---
+++ Spring/practice/src/main/java/com/greenart/practice/domain/Member.java
@@ -0,0 +1,10 @@
+package com.greenart.practice.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String name;
+    
+}
---
+++ Spring/practice/src/main/java/com/greenart/practice/domain/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package com.greenart.practice.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //멤버 저장시 일련번호 값 1 증가
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //null이라도 감싸서 반환가능
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) //같은 name을 가지고 있는 객체를 찾으면 반환. 없으면 null반환
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    
+}
