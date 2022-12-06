87aef73a43f4aaeb717d9ff0a19b8a916f6e8416
jh <bis04199@gmail.com>
2022-12-06 14:04:26 +0900

    221206 L

---
+++ Spring/practice/src/test/java/com/greenart/practice/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.greenart.practice.repository;
+
+import org.junit.jupiter.api.Test;
+
+import com.greenart.practice.domain.MemoryMemberRepository;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();   
+
+    @Test
+    public void save(){
+        
+    }
+}
