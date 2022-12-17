a4297076d0855d657ba50dd5ae2a9b042f87edea
pjh <bis04199@gmail.com>
2022-12-17 15:03:10 +0900

    221217

---
+++ kybopractice/src/main/java/com/greenart/kybopractice/controller/bookController.java
@@ -0,0 +1,33 @@
+package com.greenart.kybopractice.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+
+import com.greenart.kybopractice.service.BookService;
+import com.greenart.kybopractice.service.ReviewService;
+
+@RestController
+@RequestMapping("/api/book")
+public class bookController {
+    @Autowired BookService bService;
+    @Autowired ReviewService rService;
+    @GetMapping("/detail")
+    public ResponseEntity<Object> detailBookInfo(@RequestParam Long seq,
+        @PageableDefault(size=5, sort="reviewDate",direction = Sort.Direction.DESC)  final Pageable page
+    ){
+        Map<String, Object> map = bService.showDetailBookInfo(seq, page);
+        map.put("review", rService.showReview(seq, page));
+        return new ResponseEntity<>(map, (HttpStatus)map.get("code"));
+    }
+    
+}
---
+++ kybopractice/src/test/java/com/greenart/kybopractice/KybopracticeApplicationTests.java
@@ -0,0 +1,46 @@
+package com.greenart.kybopractice;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.greenart.kybopractice.entity.BookEntity;
+import com.greenart.kybopractice.entity.ReviewEntity;
+import com.greenart.kybopractice.repository.BookRepositroy;
+import com.greenart.kybopractice.repository.ReviewRepository;
+
+@SpringBootTest
+class KybopracticeApplicationTests {
+	@Autowired BookRepositroy bookRepo;
+	@Autowired ReviewRepository reviewRepo;
+	@Test
+	void bookTest() {
+		List<BookEntity> list = bookRepo.findAll();
+		for(BookEntity r : list){
+			System.out.println(r);
+		}
+		System.out.println(bookRepo.findBySeq(1L));
+	}
+	@Test
+	void reviewTest(){
+		List<ReviewEntity> list = reviewRepo.findAll();
+		for(ReviewEntity r : list){
+			System.out.println(r);
+		}
+		System.out.println("--------------------------------------");
+		// List<ReviewEntity> bookreview = reviewRepo.findByBookSeq(1L);
+		// for(ReviewEntity r : bookreview){
+		// 	System.out.println(r);
+		// }
+	}
+	@Test
+	void reviewdetail(){
+		// List<ReviewEntity> list = reviewRepo.findByBookSeq(2L);
+		// for(ReviewEntity r : list){
+		// 	System.out.println(r);
+		// }
+		
+	}
+}
